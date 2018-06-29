c93a7ae98a09679ddaeb95bfe24030fd5547ea1d
HarshPatel682 <patel682@purdue.edu>
2018-06-29 10:58:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/patel/simple_app_with_graph/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.patel.simple_app_with_graph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bSubmit = (Button) findViewById(R.id.bSubmit);
+        
+    }
+}
